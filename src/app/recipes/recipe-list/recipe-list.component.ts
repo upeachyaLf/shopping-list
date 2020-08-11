2fda80fcc2f48ea7dd6178e61912adc3e606b6af
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-11 19:14:06 +0545

    recipe list added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+    {
+      name: 'Pizza',
+      description: 'Cheezy meaty pizza!!',
+      imagePath:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.HygzsIuSbo6pgHy39s4rLwHaDt%26pid%3DApi&f=1',
+    },
+    {
+      name: 'Burger',
+      description: 'Double patty with cheese and bacon',
+      imagePath:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.KZUTwF3-Zw1dIJxo67Ur4QHaFj%26pid%3DApi&f=1',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
