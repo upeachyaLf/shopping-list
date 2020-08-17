fcaf8d1647109f3d21a276572275333cdcec903c
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-17 19:32:13 +0545

    binding reactive form for recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -29,7 +29,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     SelectRecipeComponent,
     RecipeEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,10 +11,36 @@ export class RecipeEditComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   isEditMode: boolean = false;
 
+  recipeForm: FormGroup;
+
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       let currentId = params['id'];
       this.isEditMode = currentId ? true : false;
     });
+
+    this.recipeForm = new FormGroup({
+      id: new FormControl(null, Validators.required),
+      name: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      imagePath: new FormControl(null, [Validators.required]),
+      ingredents: new FormArray([]),
+    });
+  }
+
+  onSubmit() {
+    console.log('submitted', this.recipeForm);
+  }
+
+  // addIngredent() {
+  //   const control = {
+  //     name: new FormControl(null, Validators.required),
+  //     amount: new FormControl(null, Validators.required),
+  //   };
+  //   (<FormArray>this.recipeForm.get('ingredents')).push(...control);
+  // }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredents')).controls;
   }
 }
