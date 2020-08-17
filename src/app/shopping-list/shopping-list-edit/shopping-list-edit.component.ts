6bd453419edf6d32664947e1f98e459fcafd9c63
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-17 16:43:48 +0545

    edit form with validation

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -2,20 +2,19 @@ import {
   Component,
   OnInit,
   ViewChild,
-  ElementRef,
   Output,
   EventEmitter,
 } from '@angular/core';
 
 import { Ingredent } from '../../common/models/ingredent.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css'],
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
-  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+  @ViewChild('f', { static: false }) editForm: NgForm;
 
   @Output() itemAdded = new EventEmitter<Ingredent>();
 
@@ -23,10 +22,9 @@ export class ShoppingListEditComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onAddClick() {
-    this.itemAdded.emit({
-      name: this.nameInput.nativeElement.value,
-      amount: this.amountInput.nativeElement.value,
-    });
+  onSubmit() {
+    console.log(this.editForm);
+    this.itemAdded.emit(this.editForm.value);
+    this.editForm.reset();
   }
 }
