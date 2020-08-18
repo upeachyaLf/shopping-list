fc510b184366c8fce7acf1a82dec60da64e5125e
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-18 18:24:23 +0545

    service for http requests

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,5 +1,5 @@
 import { Subject } from 'rxjs';
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Ingredent } from '../common/models/ingredent.model';
 
