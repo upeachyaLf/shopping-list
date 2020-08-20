99c237179f885f327b4a8f320c2ac9dd128b2f9e
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-20 11:49:28 +0545

    adding material design login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule, // App routing module should only be imported after feature routing modules
+    AppRoutingModule,
+    NoopAnimationsModule, // App routing module should only be imported after feature routing modules
   ],
   providers: [],
   bootstrap: [AppComponent],
